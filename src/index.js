9b6b0a71942430cbf250a73a2957675445c24c1f
Dan Blakeman <githubdan@gmail.com>
2018-01-20 18:09:43 +0000

    Basic funtionality and styles

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCEpBnuSKu4r4r6O6Z72roiNYVXu4_lJk4'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      })
+    });
+  }
+
+  render() {
+    return (
+        <div>
+          <SearchBar />
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList
+              onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+              videos={this.state.videos} />
+        </div>
+    );
+  }
+}
+
+ReactDom.render(<App/>, document.querySelector('.container'));
